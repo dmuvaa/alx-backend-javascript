0feb2b8ee512c632b131146d7e7477b7d776bea6
Dennis Muvaa <dmuvaa70@gmail.com>
2023-09-27 18:01:53 +0300

    create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+
+    getNumberofDepartments(employeesList) {
+        return Object.keys(employeesList).length
+    }
+  };
+}
