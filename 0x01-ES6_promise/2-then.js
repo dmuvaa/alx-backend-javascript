6fe6b96a490d8cad6b72edc2061db856adbfb99e
Dennis Muvaa <dmuvaa70@gmail.com>
2023-09-28 11:55:53 +0300

    Get a Response from an API

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,15 @@
 function handleResponseFromAPI(promise) {
-  return promise
-    .then((success) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: success };
-    })
-    .catch((error) => {
-      console.log('Error from API:', error);
-      return new Error();
-    });
+  return new Promise((resolve, reject) => {
+    promise
+      .then((success) => {
+        console.log('Got a response from the API');
+        resolve({ status: 200, body: success });
+      })
+      .catch((error) => {
+        console.log('Error from API:', error);
+        reject(new Error());
+      });
+  });
 }
 
 export default handleResponseFromAPI;
