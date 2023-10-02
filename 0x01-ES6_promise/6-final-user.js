288898979a83b25b5721e267ca164bc6c2aaf15f
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-02 15:32:44 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
-export function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-  return new Promise.allSettled([signUpPromise])
-  .then((results) => {
-    return results.map((result) => {
-      return {
-        status: result.status,
-        view: result.status === 'fulfilled' ? result.value : result.reason, 
-      };
-    });
-  });
-}
+export const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+  
+  return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+};
