9b21a3ebb8e0869d800d7c17e5a6ed1bc1afcfd7
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-02 17:09:28 +0300

    Handle multiple promises in JS

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,15 +7,11 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
   const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
 
-  // Using .map() method, ensure you return a value at each iteration
   return results.map((result) => {
-    // Check for 'fulfilled' status and return the appropriate value
     if (result.status === 'fulfilled') {
       return { status: 'fulfilled', value: result.value };
     }
-    // Check for 'rejected' status and return the appropriate value
     if (result.status === 'rejected') {
-      // Adjust error message format to align with your test's expectation
       return { status: 'rejected', value: `Error: ${result.reason.message}` };
     }
     return undefined;
