93d1d075d2d4d16bf68cfb8c46bf227ca25ddb26
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-03 00:37:35 +0300

    async function named asyncUploadUser that will call these two functions and return an object

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,21 +1,21 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 const asyncUploadUser = async () => {
-    try {
-        const photoResponse = await uploadPhoto();
-        const userResponse = await createUser();
+  try {
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
 
-        return {
-            photo: photoResponse,
-            photo: userResponse,
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            photo: null,
-            user: null,
-        };
-    }
+    return {
+      photo: photoResponse,
+      photo: userResponse,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 };
 
 export default asyncUploadUser;
