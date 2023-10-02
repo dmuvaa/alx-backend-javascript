fbb1e45c4614439f689957994cd493354ade4f30
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-02 13:28:49 +0300

    Create Iterator Object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,10 +2,10 @@ export default function createIteratorObject(report) {
   const allEmployees = Object.values(report.allEmployees).flat();
 
   return {
-    [Symbol.iterator]: function*() {
+    * [Symbol.iterator]() {
       for (const employee of allEmployees) {
         yield employee;
       }
-    }
+    },
   };
 }
