cd753c366ed06f3fc2143224dc896e138a94ad75
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-06 13:16:19 +0300

    Implement a Class with Symbols

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,12 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this[_brand], this[_motor], this[_color]);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
 
