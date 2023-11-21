1e6a3c06ea41956465ec9a00385df0ed39658ba2
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-22 01:42:41 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.trim().split('\n');
+
+      const students = [];
+      const response = [];
+      var msg = "";
+
+      for (const line of lines) {
+        const [firstname, lastname, age, field] = line.split(',');
+        if (firstname && lastname && age && field && field !== 'field') {
+          students.push({
+            firstname, lastname, age, field,
+          });
+        }
+      }
+
+      const studentCount = students.length;
+      msg = `Number of students: ${studentCount}`;
+      console.log(msg);
+      response.push(msg);
+
+      const fields = {};
+
+      for (const student of students) {
+        if (!fields[student.field]) {
+          fields[student.field] = [];
+        }
+        fields[student.field].push(student.firstname);
+      }
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          msg = `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+          console.log(msg);
+          response.push(msg);
+        }
+      }
+
+      resolve(response);
+    });
+  });
+}
+
+module.exports = countStudents;
