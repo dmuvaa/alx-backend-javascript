9e761a805324c976568bbbe43c3c8082ea87b847
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-22 16:57:51 +0300

    Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = express();
+const port = '1245';
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+  });
+
+app.get('/students', async (request, response) => {
+  try {
+    const data = await countStudents(database);
+    if (data) {
+      const formatedData = data.join('\n');
+      response.send(`This is the list of our students\n${formatedData}`);
+    } else {
+      response.status(500).send('No student data available.');
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('There was an error.');
+  }
+});
+
+app.listen(port, () => {
+  console.log('...');
+});
+
+module.exports = app;
